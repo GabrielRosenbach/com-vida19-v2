4bdcc68c56add99365420f0fdade5a2b0cb5af3d
unknown <alanreichert22@gmail.com>
2021-06-15 18:43:05 -0300

    modificado página home, add container para prevenção do virus, modificado cores dos container

--- ProgramaçãoIV/Home/home.php
+++ ProgramaçãoIV/Home/home.php
@@ -5,11 +5,8 @@
     /*aqui ira verificar se o usuario esta logado para que nao consigam burlar o sistema digitando direto na url a página home*/
         session_start();
         if((!isset ($_SESSION['login']) == true) and (!isset($_SESSION['password']) == true)){
-            echo $_SESSION['login'];
-            echo $_SESSION['password'];
             unset($_SESSION['login']);
             unset($_SESSION['password']);
-            echo "entrou";
            header('Location: ../Cadastro_Login/Cadastro_Login.html');
         }
         $logado = $_SESSION['login'];
@@ -22,11 +19,52 @@
     </head>
     <body>
         <section id="secao_principal">
+            secao principal
             
+           <section id="container_sin_pre"><!-- aqui ficará parte do sintomas, uma apresentação dos sintomas aos usuarios-->
+        
+                <div class="container_figuras"><!--container dos sintomas-->
+                    <img src="Febre.png" alt="Pessoa com Febre" width=120px class="img_sintoma1">
+                    <img src="tossir.png" alt="Pessoa com Febre" width=120px class="img_sintoma">
+                    <img src="dor_corpo.png" alt="Pessoa com Febre" width=120px class="img_sintoma">
+                    <img src="falta_ar.png" alt="Pessoa com Febre" width=120px class="img_sintoma">
+                </div>
+                <div class="container_recomendacao">
+
+                    <div class="recomendacoes">
+                        <p class="recomendacao">Se tiveres algum dos sintomas descritos ao lado,sugerimos que siga as intruções de como se proteger e também de como não transmitir a doença a outras pessoas e  pedimos que procure um médico imediatamente.</p>
+                        <p class="recomendacao">E se quiseres poderá nos informar como está sua situação logo abaixo na página</p>
+                    </div>
+
+                    <div class="medico_instrucao">
+                        <img src="Med_instrucoes.png" width=200px height=400px alt="Medico_instrucoes" class="img_med_instucao">
+                    </div>
+                </div>
+           </section> 
             
+           <section id="container_sin_pre"><!-- aqui ficará parte das precausoes, uma apresentação das precausoes a serem tomadas aos usuarios-->
+        
+                <div class="container_figuras"><!--container dos precausoes-->
+                    <img src="tossir_cotovelo.png" alt="Pessoa com Febre" width=120px class="img_sintoma1">
+                    <img src="tossir_cotovelo.png" alt="Pessoa com Febre" width=120px class="img_sintoma">
+                    <img src="tossir_cotovelo.png" alt="Pessoa com Febre" width=120px class="img_sintoma">
+                    <img src="tossir_cotovelo.png" alt="Pessoa com Febre" width=120px class="img_sintoma">
+                </div>
+                <div class="container_recomendacao">
+
+                    <div class="recomendacoes">
+                        <p class="recomendacao">Fique seguro, fique em casa</p>
+                    </div>
+
+                    <div class="medico_instrucao">
+                        <img src="Norse_instrucao.png" width=200px height=400px alt="Medico_instrucoes" class="img_med_instucao">
+                    </div>
+                </div>
+            </section> 
 
             <article id="painel"><!--CONTERÁ ESTATISTICAS CASOS NOVOS RECUPERADOS GERAL  E POR REGIOES-->
-                <section id="estatistica"><!--ESTATISTICAS CASOS RECUPERADOS CONFIRMADOS OBITOS GERAL-->
+                painel    
+            <section id="estatistica"><!--ESTATISTICAS CASOS RECUPERADOS CONFIRMADOS  GERAL-->
                     <div class="cards">
                         <?php
                           $sql_sus =  mysqli_num_rows(mysqli_query($conexao,"select * from paciente p inner join status s on p.cod_status = s.cod_status where s.des_status = 'Suspeito'"));   
@@ -134,6 +172,20 @@
                     </section>
                 </section>
             </article><!--FIM DO ARTICLE PAINEL-->
-        </section>
+
+            <section id="secao_perguntas"><!--secao das perguntas que os usuario poderao fazer aos médicos-->
+                <div class="medicos">
+                    <img src="medicos.jpg" alt="medicos" >
+                </div>
+                <div class="pergunta">
+                    <h2>Deixe uma pergunta a nossa equipe médica!</h2>
+                    <form action="">
+                        <textarea name="pergunta" rows="10" cols="40">Deixe aqui sua pergunta...
+                        </textarea>
+                    </form>
+                </div>
+            </section>
+
+        </section><!--FIM DA SECAO PRINCIPAL-->
     </body>
 </html>
